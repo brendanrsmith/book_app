a9c71fd4a6a79d71abb955e7b5ae61257a172779
Brendan Smith <brendan5320@gmail.com>
2021-01-25 14:59:21 -0800

    server setup

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # book_app
 codefellows week 3 lab 
+
+**Author**: Brendan Smith, Lauren Sierra
+**Version**: 1.0.0
+
+## Overview
+
+## Getting Started
+
+## Architecture
+
+## Change Log
+
+
+## Credits and Collaborations
+
+Number and name of feature: ________________________________
+
+Estimate of time needed to complete: _____
+
+Start time: _____
+
+Finish time: _____
+
+Actual time needed to complete: _____
--- server.js
+++ server.js
@@ -1,1 +1,29 @@
-'use strict'
+'use strict';
+
+// ==== Packages ====
+const express = require('express');
+const superagent = require('superagent');
+
+// ==== Setup ====
+const app = express();
+app.set('view engine', 'ejs'); // express handles ejs for us
+app.use(express.urlencoded({extended: true})); // decodes http, used for POST method
+
+// ==== Other global variables ====
+const PORT = process.env.PORT || 3111;
+
+// ==== Routes ==== 
+app.get('/', getHome);
+
+
+// ==== Route Callbacks ====
+function getHome(req, res) {
+    res.render('pages/index.ejs');
+}
+
+
+
+
+
+//  Start up the server 
+app.listen(3000, () => console.log('Server up on 3000'));
